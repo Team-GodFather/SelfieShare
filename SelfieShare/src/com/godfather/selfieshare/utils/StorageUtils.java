5199f36544f3d728d8c2fb56879e2ea1c3f66d95
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2014-10-16 23:37:06 +0300

    Add UserDetailActivity

---
+++ SelfieShare/src/com/godfather/selfieshare/utils/StorageUtils.java
@@ -0,0 +1,16 @@
+package com.godfather.selfieshare.utils;
+
+import java.io.File;
+
+import android.os.Environment;
+
+public final class StorageUtils {
+
+	// Standard storage location for digital camera files
+	private static final String CAMERA_DIR = "/dcim/";
+
+	public File getAlbumStorageDir() {
+		return new File(Environment.getExternalStorageDirectory() + CAMERA_DIR);
+
+	}
+}
